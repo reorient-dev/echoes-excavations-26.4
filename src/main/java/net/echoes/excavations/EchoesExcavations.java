36144cd2acf7244c2dfffbdeae928a4ee47aedfe
Orient <nesaku.kami@gmail.com>
2025-12-24 00:29:53 +0700

    Initial commit.

---
+++ src/main/java/net/echoes/excavations/EchoesExcavations.java
@@ -0,0 +1,16 @@
+package net.echoes.excavations;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchoesExcavations implements ModInitializer {
+	public static final String MOD_ID = "echoes-and-excavations";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/echoes/excavations/EchoesExcavationsClient.java
@@ -0,0 +1,10 @@
+package net.echoes.excavations;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EchoesExcavationsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
